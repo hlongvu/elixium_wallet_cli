e2c2ab0e4141eb3bd2f35fa2718aa23a38d8a2fc
long <hlongvu@gmail.com>
2018-12-12 11:10:23 +0700

    change config to default

--- config/config.exs
+++ config/config.exs
@@ -28,8 +28,6 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env()}.exs"
-config :elixium_core,
-       data_path: "/Users/long/Works/Elixir/elixium_data"
 
 
 config :logger,
